9932e0ea00fd162c3d548f4bb3f12e8b0384a9a2
Janno Põldma <janno.poldma@gmail.com>
2012-06-05 10:33:56 +0300

    Coin, Country, NominalValue models.

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class NominalValue(models.Model):
+    value = models.CharField(max_length=100, unique=True)
+
+    def __unicode__(self):
+        return self.value
+
+
+class Country(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=32, unique=True)
+    genitive = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Coin(models.Model):
+    country = models.ForeignKey(Country)
+    nominal_value = models.ForeignKey(NominalValue)
+    commemorative_year = models.IntegerField(null=True)
+    image_file_name = models.CharField(max_length=255)
+    image_content_type = models.CharField(max_length=255)
+    image_file_size = models.IntegerField()
+    collected_at = models.DateTimeField(null=True)
+    collected_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return "%s - %s" % (self.country.name, self.nominal_value.value)
