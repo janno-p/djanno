7cee99bae508227d7c9a1834aa1312a53c2a45dd
Janno Põldma <janno.poldma@gmail.com>
2012-06-10 17:36:54 +0300

    Disabled coins module urls also.

--- djanno/urls.py
+++ djanno/urls.py
@@ -4,17 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djanno.views.home', name='home'),
-    # url(r'^djanno/', include('djanno.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    #url(r'^coins/', include('coins.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^coins/$', 'coins.views.index'),
-    url(r'^coins/(?P<coin_id>\d+)/$', 'coins.views.detail'),
-    url(r'^coins/(?P<coin_id>\d+)/results/$', 'coins.views.results'),
-    url(r'^coins/(?P<coin_id>\d+)/vote/$', 'coins.views.vote'),
 )
