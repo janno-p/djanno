457c7d2785ec3f6c05dc15cc115aab63b270ea86
Janno Põldma <janno.poldma@gmail.com>
2012-06-10 17:32:10 +0300

    Disabled coins module.

--- djanno/settings.py
+++ djanno/settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'coins',
+    #'coins',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
